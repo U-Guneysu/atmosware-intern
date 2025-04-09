d4bbc9d3b3afe51e3bae333972c304a6f233d379
Umut Guneysu <umutguneysuu@gmail.com>
2025-04-09 12:08:32 +0300

    JavaScript

---
+++ JSㅤTo-Do-List/script.js
@@ -0,0 +1,48 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+  if (inputBox.value.trim() === "") {
+    alert("Lütfen bir görev giriniz.");
+    return;
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    inputBox.value = "";
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+}
+function deleteAll() {
+  listContainer.innerHTML = "";
+  localStorage.removeItem("data");
+}
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+function keyPress(event) {
+  if (event.key === "Enter") {
+    addTask();
+  }
+}
+saveData();
+showTask();
+document.addEventListener("keydown", keyPress);
